4323163de3905c9468d679972216cf472d0e15f1
Letham01 <kusanagi29@ymail.com>
2023-08-18 13:57:34 +0200

    needs protected/private stuff

--- AI_Proj.py
+++ AI_Proj.py
@@ -2,58 +2,106 @@ import openai
 from stringcolor import *
 import os
 from dotenv import load_dotenv
+#import time '''<<<<maybe for later use of sleep functions for paused text execution'''
 
 load_dotenv()
 
-
-
-# Make sure to set up your API key
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# Display welcome message to the user
 def display_welcome_message():
-    print(cs("Welcome to ChatGPT!","blue"))
+    print(cs("Welcome to WASSUP Restaurant!","blue"))
+    print(cs("Describe what you're in the mood for, and I'll suggest a dish or drink.","darkblue"))
     print(cs("Type 'quit' to exit the chat.\n","darkblue"))
 
-# Get user input
+class MenuItem:
+    def __init__(self, name, properties):
+        self.name = name
+        self.properties = properties
+        self.__description = description
+    
+    #def get_description(self):
+        #return self.__description
+
+class Dish(MenuItem):
+    def __init__(self, name, properties):
+        super().__init__(name, properties)
+
+class Drink(MenuItem):
+    def __init__(self, name, properties):
+        super().__init__(name, properties)
+
+# Sample Menu needs more shit
+menu = [
+    Dish("Steak", ["salty", "hot"]),
+    Dish("Salad", ["fresh", "cold", "gay"]),
+    Drink("Iced Tea", ["cold", "sweet", "refreshing"]),
+    Drink("Hot Coffee", ["hot", "bitter", "delicious"]),
+]
+
+# user
 def get_user_input():
     return input("You: ")
 
-# Use AI to generate a response
-def get_chatgpt_response(messages, completions:int):
+def get_suggestion(user_input):
+    matches = []
+    for item in menu:
+        if all(word in user_input for word in item.properties):
+            matches.append(item.name)
+
+    if matches:
+        return f"How about trying the {matches[0]}?"
+    else:
+        return "I'm sorry, I couldn't find an exact match. Would you like to try something else?"
+
+#openai aaaallll the way dooowwwnn
+def get_chatgpt_response(messages):
     response = openai.ChatCompletion.create(
-        model="gpt-4",  # Using gpt-3.5-turbo as an example; replace with gpt-4 if u want to
-        messages=messages,      # The ongoing conversation
+        model="gpt-4",  # gpt model name
+        messages=messages,
         temperature=0.7,
         max_tokens=150,
-        n=completions,
+        n=1,
         presence_penalty=0.5,
         frequency_penalty=0.5
     )
-    choices = [choice.message['content'] for choice in response.choices]
-    return choices
+    return response.choices[0].message['content']
 
 # The chat interface loop
 def chat_interface():
     display_welcome_message()
-    system_role = "You are Rust Cohle from the TV Series True Detective. You speak and behave like him but you curse like Samuel L. Jackson" # DEFINE SYSTEM ROLE HERE
+    system_role = "You are a waiter at WASSUP Restaurant, suggesting dishes/drinks based on customer mood. You curse like Samuel L. Jackson"
     messages = [{"role": "system", "content": system_role}] 
     while True:
         user_input = get_user_input()
         if user_input.lower() == 'quit':
             break
 
-        # Append user message to the messages list
+        # Append user message to the message list
         messages.append({"role": "user", "content": user_input})
 
-        #Get AI response and append to messages
-        responses = get_chatgpt_response(messages, 1)           # choose how many completions you want
-        print("CHAT GPT: ")
-        for resp in responses:
-            print(cs(resp,"green"))
-            messages.append({"role": "assistant", "content": resp})
-        #print(messages) #if you want system messages, uncomment that shit
+        # Check if user is looking for a suggestion
+        if any(word in user_input for word in ["salty", "hot", "cold", "sweet", "bitter", "fresh"]):
+            suggestion = get_suggestion(user_input)
+            print(cs(suggestion,"green"))
+        else:
+            # Get AI response and append to messages
+            ai_response = get_chatgpt_response(messages)
+            print(cs(ai_response,"green"))
+            messages.append({"role": "assistant", "content": ai_response})
 
 # Run the chat interface
 if __name__ == '__main__':
     chat_interface()
+
+
+# count gpt tokens
+'''
+import tiktoken
+
+def count_tokens(text):
+    enc = tiktoken.encoding_for_model("text-davinci-003")
+    tokens = len(enc.encode(text))
+    tokens_left = 4096 - tokens
+    return tokens
+'''
+
